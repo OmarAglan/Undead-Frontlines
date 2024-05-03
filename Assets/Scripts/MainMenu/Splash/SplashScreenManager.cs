d232f10ef1d178dbcc111d571e51455fc00767d7
Omar Aglan <Omar.aglan91@gmail.com>
2024-05-03 04:58:53 +0300

    Small UI Fix

--- Assets/Scripts/MainMenu/Splash/SplashScreenManager.cs
+++ Assets/Scripts/MainMenu/Splash/SplashScreenManager.cs
@@ -4,24 +4,20 @@ using UnityEngine;
 
 namespace MainMenuUICode.MainCode
 {
-    public class MangeSplashScreen : MonoBehaviour
+    public class SplashScreenManager : MonoBehaviour
     {
-        [Header("Resources")]
+        [Header("RESOURCES")]
         public GameObject splashScreen;
         public GameObject splashScreenLogin;
         public GameObject splashScreenRegister;
         public GameObject mainPanels;
         private Animator mainPanelsAnimator;
 
-        [Header("Settings")]
-        public bool isLogged;
+        [Header("SETTINGS")]
+        public bool isLoggedIn;
         public bool alwaysShowLoginScreen = true;
         public bool disableSplashScreen;
 
-        /// <summary>
-        /// Start is called on the frame when a script is enabled just before
-        /// any of the Update methods is called the first time.
-        /// </summary>
         void Start()
         {
             if (disableSplashScreen == true)
@@ -33,37 +29,37 @@ namespace MainMenuUICode.MainCode
 
                 mainPanelsAnimator = mainPanels.GetComponent<Animator>();
                 mainPanelsAnimator.Play("Main Panel Opening");
-
             }
-            else if (isLogged == false && alwaysShowLoginScreen == true)
+
+            else if (isLoggedIn == false && alwaysShowLoginScreen == true)
             {
                 splashScreen.SetActive(false);
                 splashScreenLogin.SetActive(true);
                 splashScreenRegister.SetActive(true);
             }
 
-            else if (isLogged == false && alwaysShowLoginScreen == false)
+            else if (isLoggedIn == false && alwaysShowLoginScreen == false)
             {
                 splashScreen.SetActive(false);
                 splashScreenLogin.SetActive(true);
                 splashScreenRegister.SetActive(true);
             }
 
-            else if (isLogged == false && alwaysShowLoginScreen == false)
+            else if (isLoggedIn == false && alwaysShowLoginScreen == false)
             {
                 splashScreen.SetActive(false);
                 splashScreenLogin.SetActive(true);
                 splashScreenRegister.SetActive(true);
             }
 
-            else if (isLogged == true && alwaysShowLoginScreen == true)
+            else if (isLoggedIn == true && alwaysShowLoginScreen == true)
             {
                 splashScreen.SetActive(false);
                 splashScreenLogin.SetActive(true);
                 splashScreenRegister.SetActive(true);
             }
 
-            else if (isLogged == true && alwaysShowLoginScreen == false)
+            else if (isLoggedIn == true && alwaysShowLoginScreen == false)
             {
                 splashScreen.SetActive(true);
                 splashScreenLogin.SetActive(false);
